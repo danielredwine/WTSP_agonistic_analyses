d5d25d725545e7759e5a4eda810e5212c16c89bf
Daniel Redwine <daniel.redwine@gmail.com>
2024-02-12 15:10:14 -0800

    added to graphing script

--- scripts/WTSP_graphs.R
+++ scripts/WTSP_graphs.R
@@ -20,13 +20,47 @@ agonistic_analysis_data$Agonistic_Rate <- as.numeric(agonistic_analysis_data$Ago
 
 # Clean out NA and X
 graphing_data <- agonistic_analysis_data %>%
-  select(SampleID, Winter, Wing, PCRsex, PCRMorph) %>%
+  select(SampleID, Winter, Wing, PCRsex, PCRMorph, Agonistic_Rate) %>%
   na.omit() %>%
-  filter(Winter == "FW" | "AFW")
+  filter(Winter == "FW" | Winter == "AFW") %>%
+  filter(PCRMorph == "WS" | PCRMorph == "TS") %>%
+  filter(PCRsex == "M" | PCRsex == "F")
 
+sex_boxplot <- ggplot(graphing_data, aes(PCRsex, Agonistic_Rate, fill = PCRsex)) + 
+  geom_boxplot() +
+  theme_bw() +
+  xlab("Sex") +
+  ylab("Interactions/sec") +
+  theme(legend.position  = "none")
 
+sex_boxplot
 
+morph_boxplot <- ggplot(graphing_data, aes(PCRMorph, Agonistic_Rate, fill = PCRMorph)) + 
+  geom_boxplot() +
+  theme_bw() +
+  xlab("Morph") +
+  ylab("Interactions/sec") +
+  theme(legend.position  = "none")
 
+morph_boxplot
 
+age_boxplot <- ggplot(graphing_data, aes(Winter, Agonistic_Rate, fill = Winter)) + 
+  geom_boxplot() +
+  theme_bw() +
+  xlab("Age") +
+  ylab("Interactions/sec") +
+  theme(legend.position  = "none")
 
+age_boxplot
+
+wing_scatterplot <- ggplot(graphing_data, aes(Wing, Agonistic_Rate)) + 
+  geom_point() +
+  geom_smooth(method="lm") +
+  theme_bw() +
+  ylab("Interactions/sec") +
+  xlab("Wing Chord (mm)")
+
+wing_scatterplot
+
+sex_boxplot / wing_scatterplot | morph_boxplot / age_boxplot
 
