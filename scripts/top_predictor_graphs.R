da8526565a8be88b52a72b163bc699b93e9a3b88
Daniel Redwine <daniel.redwine@gmail.com>
2024-07-12 12:59:55 -0500

    added graphs

--- scripts/top_predictor_graphs.R
+++ scripts/top_predictor_graphs.R
@@ -1,5 +1,5 @@
 # Daniel Redwine
-# WTSP Plots
+# Plots of top model predictors
 
 # Clear environment
 rm(list = ls())
@@ -20,7 +20,7 @@ target_nonzero_data <- total_data %>%
 
 # Aggressor Binomial model graph for platform time
 # Graph aggressor occurrence for platform time
-platform_time_binomial <- ggplot(total_data, aes(Platform_Time, Aggressor_Occurrence)) +
+aggressor_time_binomial <- ggplot(total_data, aes(Platform_Time, Aggressor_Occurrence)) +
   geom_point(show.legend = FALSE) + # Geom count changes size of points to count
   geom_smooth(method="glm", method.args=list(family="binomial"(link="logit")), 
               color = "slateblue", fill = "lightskyblue2") +
@@ -28,7 +28,24 @@ platform_time_binomial <- ggplot(total_data, aes(Platform_Time, Aggressor_Occurr
   xlab ("Conspecific Foraging Time (s)") +
   theme_bw()
 
-platform_time_binomial #Call object 
+aggressor_time_binomial #Call object 
+
+ggsave("output/significant_predictors/aggressor_time_binomial.png") # Save object
+
+# nonzero scatterplot for density data and rate of aggression
+# Rate is obtained by dividing total aggression by log platform time
+aggressor_density_rate_nonzero <- ggplot(aggressor_nonzero_data, 
+                            aes(Feeding_Density, Total_Agonistic/log(Platform_Time))) + 
+  geom_point() +
+  geom_smooth(method="glm.nb") +
+  theme_bw() +
+  ylab("Total Aggression/log(foraging time)") +
+  xlab("Feeding Density") +
+  coord_cartesian(xlim = c(2, 8), ylim = c(0, 1.5))
+
+aggressor_density_rate_nonzero # call object
+
+ggsave("output/significant_predictors/aggressor_density_rate_nonzero.png") # save object
 
 # Recipient age binomial proportion plot
 # Calculate proportions
@@ -37,15 +54,17 @@ age_target_proportion <- total_data %>%
   summarise(proportion = mean(Recipient_Occurrence))
 
 # Create bar plot
-age_targetted_plot <- ggplot(age_target_proportion, aes(x = Winter , y = proportion)) +
+target_age_binomial <- ggplot(age_target_proportion, aes(x = Winter , y = proportion)) +
   geom_bar(stat = "identity", position = "dodge", colour = "black", fill = "skyblue") +
   labs(x = "Age", y = "Proportion Targetted") +
   theme_bw()
 
-age_targetted_plot
+target_age_binomial # call object
+
+ggsave("output/significant_predictors/target_age_binomial.png") # save object
 
 # Graph recipient occurrence for feeding density
-density_recipient_binomial <- ggplot(total_data, aes(Feeding_Density, Recipient_Occurrence)) +
+target_density_binomial <- ggplot(total_data, aes(Feeding_Density, Recipient_Occurrence)) +
   geom_count(show.legend = FALSE) + # Geom count changes size of points to count
   geom_smooth(method="glm", method.args=list(family="binomial"(link="logit")), 
               color = "slateblue", fill = "lightskyblue2") +
@@ -53,4 +72,107 @@ density_recipient_binomial <- ggplot(total_data, aes(Feeding_Density, Recipient_
   xlab ("Feeding Density") +
   theme_bw()
 
-density_recipient_binomial #Call object 
+target_density_binomial #Call object 
+
+ggsave("output/significant_predictors/target_density_binomial.png") # save object
+
+# target binomial model graph for platform time
+# Graph target occurrence for platform time
+target_time_binomial <- ggplot(total_data, aes(Platform_Time, Recipient_Occurrence)) +
+  geom_point(show.legend = FALSE) + # Geom count changes size of points to count
+  geom_smooth(method="glm", method.args=list(family="binomial"(link="logit")), 
+              color = "slateblue", fill = "lightskyblue2") +
+  ylab ("Target of Aggression") +
+  xlab ("Conspecific Foraging Time (s)") +
+  theme_bw()
+
+target_time_binomial #Call object 
+
+ggsave("output/significant_predictors/target_time_binomial.png") # save object
+
+# nonzero scatterplot for density data and target rate
+target_density_rate_nonzero <- ggplot(target_nonzero_data, 
+                            aes(Feeding_Density, Total_Recipient/log(Platform_Time))) + 
+  geom_point() +
+  geom_smooth(method="glm.nb") +
+  theme_bw() +
+  ylab("Total Target/log(foraging time)") +
+  xlab("Feeding Density") +
+  coord_cartesian(xlim = c(2, 8), ylim = c(0, 3))
+
+target_density_rate_nonzero # call
+
+ggsave("output/significant_predictors/target_density_rate_nonzero.png") # save 
+
+
+# graph target rate by age for nonzero data 
+# create summary data 
+target_age_summary_nonzero <- target_nonzero_data %>%
+  filter(Winter == "FW" | Winter == "AFW") %>%
+  group_by(Winter) %>%
+  summarise(mean_recipient = mean(Total_Recipient/log(Platform_Time)), se_recipient = 
+              sd((Total_Recipient/log(Platform_Time))/sqrt(n())))
+
+# create bar chart 
+target_age_rate_nonzero <- ggplot(target_age_summary_nonzero, 
+                                  aes(x = Winter, y = mean_recipient)) +
+  geom_bar(stat = "identity", position = "dodge", colour = "black", fill = "skyblue") +
+  geom_errorbar(aes(ymin = mean_recipient - se_recipient,
+                    ymax = mean_recipient + se_recipient), width = 0.1) +
+  theme_bw() +
+  xlab("Age") +
+  ylab("Rate Targetted")
+
+target_age_rate_nonzero # call 
+
+ggsave("output/significant_predictors/target_age_rate_nonzero.png") # save 
+
+# graph target rate by age for all data 
+# create summary
+target_age_summary <- total_data %>%
+  filter(Winter == "FW" | Winter == "AFW") %>%
+  group_by(Winter) %>%
+  summarise(mean_recipient = mean(Total_Recipient/log(Platform_Time)), se_recipient = 
+              sd((Total_Recipient/log(Platform_Time))/sqrt(n())))
+
+# create bar chart
+target_age_rate <- ggplot(target_age_summary, 
+                                  aes(x = Winter, y = mean_recipient)) +
+  geom_bar(stat = "identity", position = "dodge", colour = "black", fill = "skyblue") +
+  geom_errorbar(aes(ymin = mean_recipient - se_recipient,
+                    ymax = mean_recipient + se_recipient), width = 0.1) +
+  theme_bw() +
+  xlab("Age") +
+  ylab("Rate Targetted")
+
+target_age_rate # call
+
+ggsave("output/significant_predictors/target_age_rate.png") # save 
+
+#scatterplot for density data and target rate for all data 
+target_density_rate <- ggplot(total_data, 
+                            aes(Feeding_Density, Total_Recipient/log(Platform_Time))) + 
+  geom_point() +
+  geom_smooth(method="glm.nb") +
+  theme_bw() +
+  ylab("Total Target/log(foraging time)") +
+  xlab("Feeding Density") +
+  coord_cartesian(xlim = c(2, 8), ylim = c(0, 3))
+
+target_density_rate # call
+
+ggsave("output/significant_predictors/target_density_rate.png") # save 
+
+# scatterplot for wing data adjusted
+target_wing_rate <- ggplot(total_data, 
+                           aes(adjusted_wing, Total_Recipient/log(Platform_Time))) + 
+  geom_count(show.legend = FALSE) +
+  geom_smooth(method="glm.nb") +
+  theme_bw() +
+  ylab("Rate Targetted") +
+  xlab("Adjusted Wing") +
+  coord_cartesian(ylim = c( 0 , 2))
+
+target_wing_rate # call
+
+ggsave("output/significant_predictors/target_wing_rate.png") # save
